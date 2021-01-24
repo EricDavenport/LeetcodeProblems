70bb3ff489d3f0e7e290d9523a90c225f90a3041
Eric Davenport <edavenport6791@gmail.com>
2021-01-24 07:02:50 -0500

    Latest possible problem 1736 added

---
+++ LeetcodeDSA.playground/Pages/LatestTime.xcplaygroundpage/Contents.swift
@@ -0,0 +1,71 @@
+import Foundation
+
+/*
+ You are given a string time in the form of hh:mm, where some of the digits in the string are hidden (represented by ?).
+ 
+ The valid times are those inclusively between 00:00 and 23:59.
+ 
+ Return the latest valid time you can get from time by replacing the hidden digits.
+ 
+ Example 1:
+ Input: time = "2?:?0"
+ Output: "23:50"
+ Explanation: The latest hour beginning with the digit '2' is 23 and the latest minute ending with the digit '0' is 50.
+ 
+ Example 2:
+ Input: time = "0?:3?"
+ Output: "09:39"
+ 
+ Example 3:
+ Input: time = "1?:22"
+ Output: "19:22"
+ 
+ Constraints:
+ time is in the format hh:mm.
+ It is guaranteed that you can produce a valid time from the given string.
+ */
+
+
+func maximumTime(_ time: String) -> String {
+  var realTime = ""
+  var index = 0
+
+  for i in time {
+    if i == "?" {
+      print(i)
+    }
+  }
+  
+  return realTime
+}
+
+print(maximumTime("23:??"))
+
+
+/*
+ 
+//  if time[0] == "?" {
+//    if Int(time[1]) < 4 || time[1] == "?" {
+//      realTime.append("2")
+//    } else if Int(time[1]) >= 4 {
+//      realTime.append("1")
+//    }
+//  }
+//
+//  if time[1] == "?" {
+//    if time[0] == "0" || time[0] == "1" {
+//      realTime.append("9")
+//    }
+//    else if time[0] == "2" {
+//      realTime.append("3")
+//    }
+//  }
+//
+//  if time[3] == "?" {
+//    realTime.append("5")
+//  }
+//
+//  if time[4] == "?" {
+//    realTime.append("9")
+//  }
+ */
--- LeetcodeDSA.playground/Pages/ReverseInteger.xcplaygroundpage/Contents.swift
+++ LeetcodeDSA.playground/Pages/ReverseInteger.xcplaygroundpage/Contents.swift
@@ -1,7 +1,7 @@
 import Foundation
 
 // REVERSE INTEGER
-// Problem 
+// Problem 7
 /*
  Given a signed 32-bit integer x, return x with its digits reversed. If reversing x causes the value to go outside the signed 32-bit integer range [-231, 231 - 1], then return 0.
 
